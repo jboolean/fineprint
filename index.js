d6e24c4af08d627e72a6d85311cd056561a1380b
Julian Boilen <julianboilen@gmail.com>
2019-03-17 21:18:13 -0400

    Use vimeo SDK

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const urls = {
   forgiveness: 'https://player.vimeo.com/video/247727588' + videoUrlOptions
 }
 
+let activePlayer;
+
 const showVideo = (id) => {
   var aboveFold = document.getElementById('aboveFold');
   aboveFold.classList.remove('videoHidden');
@@ -12,7 +14,23 @@ const showVideo = (id) => {
   aboveFold.classList.remove('videoHidden', ...shownClasses);
   aboveFold.classList.add('videoVisible', id + 'Shown')
   const url = urls[id];
-  document.getElementById('vimeoFrame').setAttribute('src', url);
+  // document.getElementById('vimeoFrame').setAttribute('src', url);
+
+  // const videoContainer = document.getElementById('videoContainer');
+  if (activePlayer) {
+    activePlayer.pause();
+  }
+
+  activePlayer = new Vimeo.Player('vimeoRenderBox', {
+    url,
+    byline: false,
+    color: 'fff',
+    title: false,
+    portrait: false,
+    responsive: true
+  });
+
+  vimeoPlayer.play();
 }
 
 const bind = () => {
